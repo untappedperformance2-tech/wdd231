62c01a5f897da22b960703536077b64cf70a7e9a
untappedperformance2-tech <untappedperformance2@gmail.com>
2026-05-06 18:40:15 -0600

    Add dynamic copyright and last modified dates

---
+++ week01/Course Home Page/scripts/dates.js
@@ -0,0 +1,5 @@
+// Dynamic copyright year
+document.getElementById('currentyear').innerHTML = new Date().getFullYear();
+
+// Last modified date
+document.getElementById('lastModified').innerHTML = "Last modified: " + document.lastModified;
